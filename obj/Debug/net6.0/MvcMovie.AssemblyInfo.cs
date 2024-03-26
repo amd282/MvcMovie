b2adf08c4b613a6d39116bfc7692930df345a624
amd282 <amd282@yahoo.com>
2024-03-26 16:54:46 -0400

    added search by genre

--- obj/Debug/net6.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net6.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9caa0c9b9ab661dea842938ce9f7d9de06855c26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c86b243a0f8c8ead833c7524a23a25383289bc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
